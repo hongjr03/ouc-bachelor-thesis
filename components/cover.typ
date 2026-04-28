hongjr03/ouc-bachelor-thesis-template

commit ffe53a2c2d63801770d792c4bf437c486ea50c3a

feat: multi advisor

components/cover.typ
@@ -1,4 +1,5 @@
 #import "@preview/pointless-size:0.1.2": zh
+#import "@preview/cuti:0.4.0": fakebold
 #import "/components/logo.typ": logo
 
 #let empty-underline(width: 100%) = box("", width: width, stroke: (bottom: 0.5pt))
@@ -15,8 +16,8 @@
       row-gutter: .65em,
       stroke: none,
       align: center,
-      [*分类号*], empty-underline(),
-      [*密级*], empty-underline(),
+      fakebold[分类号], empty-underline(),
+      fakebold[密级], empty-underline(),
       [*UDC*], table.cell(colspan: 3, empty-underline()),
     ),
     place(right + top, dy: -2em, logo),
@@ -26,14 +27,14 @@
 #let cover-title(fonts: (:), title: "") = {
   set text(font: fonts.宋体, size: zh("小初"))
 
-  align(center, strong(
+  align(center, fakebold(
     "本科毕业论文".clusters().join(h(.4em)) + "（设计）",
   ))
 
   v(0.5cm)
 
 
-  place(center, text(font: fonts.黑体, size: zh("一号"), if type(title) == str { title } else { title.join("\n") }))
+  align(center, text(font: fonts.黑体, size: zh("一号"), if type(title) == str { title } else { title.join("\n") }))
 }
 
 #let underline-box(c, width, stroke: (bottom: 0.5pt)) = box(width: 1fr, move(dy: .21em, box(
@@ -92,15 +93,15 @@
   {
     cover-info(fonts: fonts)
 
-    v(6.77cm)
+    v(6.27cm)
 
     cover-title(fonts: fonts, title: title)
 
-    v(7.15cm)
+    v(4.65cm)
 
     thesis-info(
       author: (name: name, id: id),
-      advisor: advisor,
+      advisor: if type(advisor) == array { advisor.join(h(1em)) } else { advisor },
       college: college,
       department: department,
       fonts: fonts,
@@ -108,7 +109,7 @@
 
     v(.2cm)
 
-    align(center, strong(text(font: fonts.楷体, size: zh("小二"), "中国海洋大学".clusters().join(h(.5em)))))
+    align(center, fakebold(text(font: fonts.楷体, size: zh("小二"), "中国海洋大学".clusters().join(h(.5em)))))
   },
   // background: image("/1d6f220a-7701-49c4-8084-16a8f20fb41a(1).pdf", page: 5),
 )

template.typ
@@ -15,7 +15,7 @@
     name: z.string(),
     id: z.string(),
   )),
-  advisor: z.string(),
+  advisor: z.either(z.string(), z.array(z.string())),
   college: z.string(),
   department: z.string(),
   abstract: z.dictionary((
@@ -38,6 +38,7 @@
   department: "",
   abstract: (:),
   keywords: (:),
+  fonts: default-fonts,
   bibliography: "",
   acknowledgments: [],
   config: (:),
@@ -59,7 +60,7 @@
   )
 
   // 把传入的纯字体名称转换为带有中西文 fallback 配置的实际可用字体数组
-  let fonts = default-fonts + config.at("fonts", default: ())
+  let fonts = default-fonts + fonts
   let resolved-fonts = setup-fonts(fonts)
 
   let title-str = (zh: if type(title.zh) == str { title.zh } else { title.zh.join() }, en: title.en)
@@ -71,9 +72,6 @@
     description: abstract.zh,
   )
 
-  // 全局样式
-  show: global-style.with(fonts: resolved-fonts)
-
   // 封面
   cover(
     title: title.zh,
@@ -84,6 +82,9 @@
     department: department,
     fonts: resolved-fonts,
   )
+
+  // 全局样式
+  show: global-style.with(fonts: resolved-fonts)
 
   // 摘要
   a.abstract(
@@ -97,17 +98,14 @@
   outline()
 
   // 正文样式与内容
-  show: apply-style.with(
-    title: title-str.zh,
-    chap-num-config: config.at("numbering", default: ()),
-  )
+  show: apply-style.with(title: title-str.zh, chap-num-config: config.at("numbering", default: ()))
 
 
   body
 
   pagebreak(weak: true)
 
-  std.bibliography(bytes(bibliography), style: "gb-7714-2015-numeric", full: false)
+  std.bibliography(bytes(bibliography), style: "gb-7714-2015-numeric", full: true)
 
   acks(acknowledgments)
 }

template/main.typ
@@ -6,7 +6,7 @@
     en: "The Practice of Dance Based on Singing, Dancing, Rapping and Basketball",
   ),
   author: (name: "蔡徐坤", id: "123456789"),
-  advisor: "唱跳导师",
+  advisor: ("唱跳导师", "篮球导师"),
   college: "信息科学与工程学院",
   department: "计算机科学与技术2017级",
   abstract: (

hongjr03/ouc-bachelor-thesis

commit 6eb9ed7faa6ae2be11c069109bc5be8a876c5497

fix: regression in ffe53a2c2d63801770d792c4bf437c486ea50c3a

components/cover.typ
@@ -1,5 +1,4 @@
 #import "@preview/pointless-size:0.1.2": zh
-#import "@preview/cuti:0.4.0": fakebold
 #import "/components/logo.typ": logo
 
 #let empty-underline(width: 100%) = box("", width: width, stroke: (bottom: 0.5pt))
@@ -16,8 +15,8 @@
       row-gutter: .65em,
       stroke: none,
       align: center,
-      fakebold[分类号], empty-underline(),
-      fakebold[密级], empty-underline(),
+      [*分类号*], empty-underline(),
+      [*密级*], empty-underline(),
       [*UDC*], table.cell(colspan: 3, empty-underline()),
     ),
     place(right + top, dy: -2em, logo),
@@ -27,14 +26,14 @@
 #let cover-title(fonts: (:), title: "") = {
   set text(font: fonts.宋体, size: zh("小初"))
 
-  align(center, fakebold(
+  align(center, strong(
     "本科毕业论文".clusters().join(h(.4em)) + "（设计）",
   ))
 
   v(0.5cm)
 
 
-  align(center, text(font: fonts.黑体, size: zh("一号"), if type(title) == str { title } else { title.join("\n") }))
+  place(center, text(font: fonts.黑体, size: zh("一号"), if type(title) == str { title } else { title.join("\n") }))
 }
 
 #let underline-box(c, width, stroke: (bottom: 0.5pt)) = box(width: 1fr, move(dy: .21em, box(
@@ -93,11 +92,11 @@
   {
     cover-info(fonts: fonts)
 
-    v(6.27cm)
+    v(6.77cm)
 
     cover-title(fonts: fonts, title: title)
 
-    v(4.65cm)
+    v(7.15cm)
 
     thesis-info(
       author: (name: name, id: id),
@@ -109,7 +108,7 @@
 
     v(.2cm)
 
-    align(center, fakebold(text(font: fonts.楷体, size: zh("小二"), "中国海洋大学".clusters().join(h(.5em)))))
+    align(center, strong(text(font: fonts.楷体, size: zh("小二"), "中国海洋大学".clusters().join(h(.5em)))))
   },
   // background: image("/1d6f220a-7701-49c4-8084-16a8f20fb41a(1).pdf", page: 5),
 )

template.typ
@@ -38,7 +38,6 @@
   department: "",
   abstract: (:),
   keywords: (:),
-  fonts: default-fonts,
   bibliography: "",
   acknowledgments: [],
   config: (:),
@@ -60,7 +59,7 @@
   )
 
   // 把传入的纯字体名称转换为带有中西文 fallback 配置的实际可用字体数组
-  let fonts = default-fonts + fonts
+  let fonts = default-fonts + config.at("fonts", default: ())
   let resolved-fonts = setup-fonts(fonts)
 
   let title-str = (zh: if type(title.zh) == str { title.zh } else { title.zh.join() }, en: title.en)
@@ -72,6 +71,9 @@
     description: abstract.zh,
   )
 
+  // 全局样式
+  show: global-style.with(fonts: resolved-fonts)
+
   // 封面
   cover(
     title: title.zh,
@@ -82,9 +84,6 @@
     department: department,
     fonts: resolved-fonts,
   )
-
-  // 全局样式
-  show: global-style.with(fonts: resolved-fonts)
 
   // 摘要
   a.abstract(
@@ -98,14 +97,17 @@
   outline()
 
   // 正文样式与内容
-  show: apply-style.with(title: title-str.zh, chap-num-config: config.at("numbering", default: ()))
+  show: apply-style.with(
+    title: title-str.zh,
+    chap-num-config: config.at("numbering", default: ()),
+  )
 
 
   body
 
   pagebreak(weak: true)
 
-  std.bibliography(bytes(bibliography), style: "gb-7714-2015-numeric", full: true)
+  std.bibliography(bytes(bibliography), style: "gb-7714-2015-numeric", full: false)
 
   acks(acknowledgments)
 }